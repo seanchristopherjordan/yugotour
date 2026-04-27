f0e3789c5c0b0ce3c0cd4067b890cf3e7ac1b533
seanchristopherjordan <31063175+seanchristopherjordan@users.noreply.github.com>
2026-04-27 17:37:20 +0200

    fix: getting vercel up to date with images

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ import { redirects } from './redirects'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+  : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3001'
 
 
 const nextConfig: NextConfig = {
@@ -34,6 +34,8 @@ const nextConfig: NextConfig = {
           protocol: url.protocol.replace(':', '') as 'http' | 'https',
         }
       }),
+      { hostname: 'utfs.io', protocol: 'https' as const },
+      { hostname: '*.ufs.sh', protocol: 'https' as const },
     ],
   },
   webpack: (webpackConfig) => {
