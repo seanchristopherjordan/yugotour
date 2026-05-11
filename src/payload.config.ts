e2b374bd160f126a08974693be525924c63984a9
seanchristopherjordan <31063175+seanchristopherjordan@users.noreply.github.com>
2026-05-11 14:46:55 +0200

    FIxing the forgot password flow URL

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -33,6 +33,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: getServerSideURL(),
   admin: {
     meta: {
       titleSuffix: '- Yugotour',
