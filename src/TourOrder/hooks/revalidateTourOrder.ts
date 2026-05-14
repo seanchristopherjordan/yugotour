7165b65cef98b6660212028289db5a7f21fdfe93
seanchristopherjordan <31063175+seanchristopherjordan@users.noreply.github.com>
2026-05-14 11:23:33 +0200

    tour ordering fix

--- src/TourOrder/hooks/revalidateTourOrder.ts
+++ src/TourOrder/hooks/revalidateTourOrder.ts
@@ -3,6 +3,6 @@ import { revalidateTag } from 'next/cache'
 
 export const revalidateTourOrder: GlobalAfterChangeHook = ({ doc, req: { payload } }) => {
   payload.logger.info('Revalidating booking tours cache after tour order update')
-  revalidateTag('tour-order')
+  revalidateTag('tour-order', {})
   return doc
 }
