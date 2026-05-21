0965fcf53309e5a855bb8788b54839d0e8c63f93
seanchristopherjordan <31063175+seanchristopherjordan@users.noreply.github.com>
2026-05-21 13:48:47 +0200

    update sliders again

--- src/collections/Sliders/hooks/revalidateSlider.ts
+++ src/collections/Sliders/hooks/revalidateSlider.ts
@@ -3,12 +3,12 @@ import { revalidateTag } from 'next/cache'
 
 export const revalidateSlider: CollectionAfterChangeHook = ({ doc, req: { payload } }) => {
   payload.logger.info(`Revalidating slider cache after update: ${doc?.name}`)
-  revalidateTag('sliders')
+  revalidateTag('sliders', {})
   return doc
 }
 
 export const revalidateSliderDelete: CollectionAfterDeleteHook = ({ doc, req: { payload } }) => {
   payload.logger.info(`Revalidating slider cache after delete: ${doc?.name}`)
-  revalidateTag('sliders')
+  revalidateTag('sliders', {})
   return doc
 }
